b98b63f4e1cb0207026de429f701d92e596e3797
Paco Pedroche <pedroche@mat.upv.es>
2026-05-22 11:25:59 +0200

    Fix typo in sidebar news link text

--- proveta-1.py
+++ proveta-1.py
@@ -134,7 +134,7 @@ with st.sidebar:
 
 
 with st.sidebar:
-	st.markdown("Notocias sobre el método Prensa [Antena 3](https://www.antena3.com/noticias/deportes/futbol/revolucionario-metodo-matematico-puntuar-liga-estilo-formula-1_2024081266ba2e284d41750001187df6.html)", unsafe_allow_html=True)
+	st.markdown("Noticias sobre el método en Prensa [Antena 3](https://www.antena3.com/noticias/deportes/futbol/revolucionario-metodo-matematico-puntuar-liga-estilo-formula-1_2024081266ba2e284d41750001187df6.html)", unsafe_allow_html=True)
 	
 
 
