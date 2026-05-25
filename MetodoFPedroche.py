bebfedb5e3161bad2c6b9860ff8b71d90261a285
Paco Pedroche <pedroche@mat.upv.es>
2026-05-25 14:12:14 +0200

    Update MetodoFPedroche.py

--- MetodoFPedroche.py
+++ MetodoFPedroche.py
@@ -108,7 +108,7 @@ with st.sidebar:
     parVariable=st.selectbox('Equipo a estudiar',options=opciones_ordenada)  #con esa parvariable puedo hacer algo en el futuro
 
 st.subheader(f"Posiciones del {parVariable} según el método") 
-st.image(f"escudos/{parVariable}.png", caption="Escudo")
+st.image(f"escudos/{parVariable}.png", caption="")
 
 #st.markdown(f"Clasificaciones del {parVariable}: ![Escudo](escudos/{parVariable}.png)")
 
