a284b7cf47920fb3e18a7828c007742b61fc4149
Paco Pedroche <pedroche@mat.upv.es>
2026-05-25 11:40:07 +0200

    Update MetodoFPedroche.py

--- MetodoFPedroche.py
+++ MetodoFPedroche.py
@@ -31,7 +31,7 @@ with st.expander("Selecciona temporada y jornada en el menú de la izquierda. Ha
 				 "que más goles haya marcado. En caso de empate en DG y G, se prioriza al equipo con mejor clasificación "
 				 "en la jornada anterior. A continuación, en la clasificación vienen los equipos que hayan empatado, "
 				 "ordenados según el número de goles en el encuentro. Por último, vienen los  equipos que han perdido,"
-				 "también ordenados según el número de goles del encuentro. En caso de empate en el orden, se mira"
+				 "también ordenados según el número de goles del encuentro. En caso de empate en el orden, se mira "
 				 "a la clasificación de la jornada anterior y se prioriza al equipo con mejor orden.")
 
 
